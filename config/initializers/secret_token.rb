3debcf7be8e8d593c3c8257f41b9e29f7230a612
Ron Marom <ron.marom.78@gmail.com>
2013-05-14 18:09:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ronapp::Application.config.secret_token = '5b622cf1efaf21fdbd671343c9f8b00b23cd0a9b16d2c35e49008e7ed3c7dc992980d4f325964a6993f9695f3a66490c8336ab5a7e2ef937267a427b782877b8'
